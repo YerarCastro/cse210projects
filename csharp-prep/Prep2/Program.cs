1c07228f289c4a119d8fb0bee8b4ee6fb05243eb
YerarCastro <yerarcastro23@gmail.com>
2024-10-12 13:55:00 -0600

    activity 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage?: ");
+        string unserInput= Console.ReadLine() ;
+        int percentGrade = int.Parse (unserInput);
+
+        string letter = " ";
+
+        if (percentGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentGrade >= 80)
+        {
+            letter = "B";
+            
+        }
+        else if (percentGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (percentGrade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Final Grade is {letter}.");
+        
+        if (percentGrade >= 70)
+        {
+            Console.WriteLine ("You have passed. Congratulations!");
+        }
+        else 
+        {
+            Console.WriteLine ("Better luck next time!"); 
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }   
 }
