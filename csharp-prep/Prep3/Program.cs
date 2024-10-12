ea76b44be1f3d6c6b5887deab5478f278adada36
YerarCastro <yerarcastro23@gmail.com>
2024-10-12 17:54:16 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magical number?");
+        string userNumber = Console.ReadLine ();
+        float magicNumber = float.Parse(userNumber);
+        
+
+                float guess = -1;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = float.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+    
     }
+
 }
